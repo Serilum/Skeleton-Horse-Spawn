83614f9d0ab579015680e7e7ed465219daf27a54
Rick South <rick@natamus.com>
2024-06-14 05:12:02 +0200

    Skeleton Horse Spawn source code for 1.21.0.

---
+++ Fabric/src/main/java/com/natamus/skeletonhorsespawn/ModFabric.java
@@ -0,0 +1,39 @@
+package com.natamus.skeletonhorsespawn;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.skeletonhorsespawn.events.SkeletonHorseEvent;
+import com.natamus.skeletonhorsespawn.util.Reference;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		ServerTickEvents.START_WORLD_TICK.register((ServerLevel world) -> {
+			SkeletonHorseEvent.onWorldTick(world);
+		});
+
+		ServerEntityEvents.ENTITY_LOAD.register((Entity entity, ServerLevel world) -> {
+			SkeletonHorseEvent.onEntityJoin(world, entity);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
