d6732c5abe9e0a6f2accae58789462dd1dc56102
Rick South <rick@natamus.com>
2024-07-28 07:15:14 +0200

    (v4.0) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Common/src/main/java/com/natamus/skeletonhorsespawn/util/Reference.java
+++ Common/src/main/java/com/natamus/skeletonhorsespawn/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.skeletonhorsespawn.util;
 public class Reference {
 	public static final String MOD_ID = "skeletonhorsespawn";
 	public static final String NAME = "Skeleton Horse Spawn";
-	public static final String VERSION = "3.9";
+	public static final String VERSION = "4.0";
 	public static final String ACCEPTED_VERSIONS = "[1.21.0]";
 }
--- NeoForge/src/main/java/com/natamus/skeletonhorsespawn/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/skeletonhorsespawn/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.skeletonhorsespawn;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.skeletonhorsespawn.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.skeletonhorsespawn.neoforge.events.NeoForgeSkeletonHorseEvent;
 import com.natamus.skeletonhorsespawn.util.Reference;
@@ -14,6 +15,10 @@ import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
