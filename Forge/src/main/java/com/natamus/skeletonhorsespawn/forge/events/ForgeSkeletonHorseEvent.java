b9aa512ee32610c92c958def5e5f3d85cfc67fdf
Rick South <rick@natamus.com>
2025-07-18 02:43:01 +0200

    Skeleton Horse Spawn source code for 1.21.8.

---
+++ Forge/src/main/java/com/natamus/skeletonhorsespawn/forge/events/ForgeSkeletonHorseEvent.java
@@ -0,0 +1,33 @@
+package com.natamus.skeletonhorsespawn.forge.events;
+
+import com.natamus.skeletonhorsespawn.events.SkeletonHorseEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.TickEvent.LevelTickEvent;
+import net.minecraftforge.event.TickEvent.Phase;
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
+import net.minecraftforge.eventbus.api.bus.BusGroup;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+
+import java.lang.invoke.MethodHandles;
+
+public class ForgeSkeletonHorseEvent {
+	public static void registerEventsInBus() {
+		BusGroup.DEFAULT.register(MethodHandles.lookup(), ForgeSkeletonHorseEvent.class);
+	}
+
+	@SubscribeEvent
+	public static void onEntityJoin(EntityJoinLevelEvent e) {
+		SkeletonHorseEvent.onEntityJoin(e.getLevel(), e.getEntity());
+	}
+	
+	@SubscribeEvent
+	public static void onWorldTick(LevelTickEvent.Post e) {
+		Level level = e.level;
+		if (level.isClientSide) {
+			return;
+		}
+		
+		SkeletonHorseEvent.onWorldTick((ServerLevel)level);
+	}
+}
